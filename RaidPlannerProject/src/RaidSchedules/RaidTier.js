2387628ef9838d56da29d737755aae247f3f7be8
Randy Schouten <randy.schouten@mediatonic.co.uk>
2022-08-20 20:56:52 +0200

    Added teams page, nav bar

--- RaidPlannerProject/src/RaidSchedules/RaidTier.js
+++ RaidPlannerProject/src/RaidSchedules/RaidTier.js
@@ -2,41 +2,43 @@ import React from 'react';
 import RaidTierRow from './RaidTierRow';
 
 class RaidTier extends React.Component {
-    render(){
-        return(
+    render() {
+        return (
             <div className='raid-tier'>
-                {this.props.tier.printNames == true &&
+                {this.props.tier.printNames === true &&
                     <div className='raid-names-label-padder'>
-                        <div className='raid-names-label raid-names-label-padder'/>
+                        <div className='raid-names-label raid-names-label-padder' />
                         <h2 className='raid-title'>{this.props.tier.name}</h2>
                     </div>
                 }
 
-                {this.props.tier.printNames == false && 
+                {this.props.tier.printNames === false &&
                     <h2 className='raid-title'>{this.props.tier.name}</h2>
                 }
                 <table className='raid-table'>
-                    <tr>
-                        {this.props.tier.printNames == true && 
-                            <th className='raid-names-label'/>
-                        }
-                        
-                        {this.props.tier.raidDefs.map((raid, index) => {
-                        return <th>
-                            <div className='raid-header'>
-                                <div>{raid.acronym ? raid.acronym : raid.name}</div>
-                                <div className='mini-subtitle'>{raid.subtitle}</div>
-                                <div className='raid-difficulty'>
-                                    <div>Normal</div>
-                                    {!this.props.tier.hideHardMode &&
-                                        <div>{this.props.tier.hardPrefix}</div>
-                                    }
-                                </div>
-                            </div>
-                        </th>
-                    })}
-                </tr>
-                    <RaidTierRow tier={this.props.tier} raiders={this.props.raiders}/>
+                    <tbody>
+                        <tr>
+                            {this.props.tier.printNames === true &&
+                                <th className='raid-names-label' />
+                            }
+
+                            {React.Children.toArray(this.props.tier.raidDefs.map((raid, index) => {
+                                return <th>
+                                    <div className='raid-header'>
+                                        <div>{raid.acronym ? raid.acronym : raid.name}</div>
+                                        <div className='mini-subtitle'>{raid.subtitle}</div>
+                                        <div className='raid-difficulty'>
+                                            <div>Normal</div>
+                                            {!this.props.tier.hideHardMode &&
+                                                <div>{this.props.tier.hardPrefix}</div>
+                                            }
+                                        </div>
+                                    </div>
+                                </th>
+                            }))}
+                        </tr>
+                        <RaidTierRow tier={this.props.tier} raiders={this.props.raiders} />
+                    </tbody>
                 </table>
             </div>
         );
